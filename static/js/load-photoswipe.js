c88845799b574135fa472a623be4a4ac99e11a19
ksaleem <ksaleem@digitalocean.com>
2023-05-20 21:49:59 -0400

    fixes

--- static/js/load-photoswipe.js
+++ static/js/load-photoswipe.js
@@ -1,8 +1,3 @@
-/*
-Put this file in /static/js/load-photoswipe.js
-Documentation and licence at https://github.com/liwenyip/hugo-easy-gallery/
-*/
-
 /* Show an alert if this js file has been loaded twice */
 if (window.loadphotoswipejs) {
 	window.alert("You've loaded load-photoswipe.js twice. See https://github.com/liwenyip/hugo-easy-gallery/issues/6")
